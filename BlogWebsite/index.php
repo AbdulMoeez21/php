cd5da1faeeed2645304be11a4466af603053f5d0
Abdul Moiz <moizrao7@gmail.com>
2022-08-18 00:13:38 +0500

    front page

---
+++ BlogWebsite/index.php
@@ -0,0 +1,61 @@
+<!-- https://codepen.io/loracsilva/pen/ZrRYVL -->
+<?php 
+include ('./includes/Header.php');
+?>
+
+
+
+<div class="container">
+<!-- navbar -->
+
+<nav class="nav bg-secondary p-2">
+  <a class="nav-link text-info active" aria-current="page" href="#">Active</a>
+  <a class="nav-link text-info" href="#">Link</a>
+  <a class="nav-link text-info" href="#">Link</a>
+  
+</nav>
+<!-- slider -->
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./imgs/1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./imgs/2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./imgs/3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+<!-- cards -->
+
+<div class="card m-5" style="width: 18rem;">
+  <img src="./imgs/1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<?php
+include ('./includes/Footer.php'); 
+?>
