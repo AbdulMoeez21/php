86e419cd7f1d7390fa7dc08ea0c655a70d0d25a6
abar ahmed <abrarahmed2185@gmail.com>
2022-08-18 22:51:48 +0500

    Boostrap Integration

--- BlogWebsite/index.php
+++ BlogWebsite/index.php
@@ -1,7 +1,8 @@
 <!-- https://codepen.io/loracsilva/pen/ZrRYVL -->
-<?php 
-include ('./includes/Header.php');
+<?php
+include('./includes/Header.php');
 ?>
+
 
 
 
@@ -41,78 +42,34 @@ include ('./includes/Header.php');
 <!-- cards -->
 
 <div class="container">
-<div class="row d-flex flex-row">
+  <div class="row d-flex flex-row">
 
-<div class="col-4 ">
-<div class="card m-5">
-  <img src="./imgs/1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="col-12 col-md-4 ">
+      <div class="card m-5">
+        <img src="./imgs/1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-4 ">
+      <div class="card m-5">
+        <img src="./imgs/1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+
   </div>
-</div>
-</div>
-
-<div class="col-4">
-<div class="card m-5">
-  <img src="./imgs/1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-
-<div class="col-4">
-<div class="card m-5" >
-  <img src="./imgs/1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-
-<div class="col-4 ">
-<div class="card m-5">
-  <img src="./imgs/1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-
-<div class="col-4">
-<div class="card m-5" >
-  <img src="./imgs/1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-
-<div class="col-4 ">
-<div class="card m-5">
-  <img src="./imgs/1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-
-
-</div>
 </div>
 
 <?php
-include ('./includes/Footer.php'); 
+include('./includes/Footer.php');
 ?>
